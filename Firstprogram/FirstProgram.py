479f8c4a0b515bbc25f79e5a135bc7b6051805ae
RewantRawat <rewantrawat1596@gmail.com>
2026-02-18 12:47:17 +0530

    first commit

---
+++ Firstprogram/FirstProgram.py
@@ -0,0 +1,1 @@
+print("Hello there i am here!")
---
+++ Variabl;e/variable.py
@@ -0,0 +1,30 @@
+name ="Rewant Rawat"
+age = "21"
+
+print(name)
+
+
+name = "Rewant"
+age = 21
+price = 99.5
+is_dev = True
+
+print(type(age))
+
+first_name="tony"
+last_name="stark"
+age1= 51
+is_genius=True
+print(first_name,last_name,"is",age1,"years old and genius:",is_genius)
+
+name="Vishesh saini"
+age=22
+course="BCA"
+fees = 45000
+passed=True
+
+print("Student:", name)
+print("Age:", age)
+print("Course:", course)
+print("Fees:", fees)
+print("Passed:", passed)
